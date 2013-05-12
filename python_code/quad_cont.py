57711283292ae31dd197497b904436d80d84387c
Main <main@mit.edu>
2013-05-12 09:30:16 -0400

    Updated quadrotor joystick control python code. Joystick parameters and multipliers are closer to correct, though they still need to be fine tuned.

--- python_code/quad_cont.py
+++ python_code/quad_cont.py
@@ -3,7 +3,6 @@ import pygame
 import serial
 import time
 
-# Just a bit of boilerplate to start from next time
 
 pygame.init()
 pygame.joystick.init()
@@ -21,7 +20,7 @@ def quadprint():
         pygame.event.get()
         for axis in range(joy.get_numaxes()):
             position = joy.get_axis(axis)
-            print axis_convert(axis, position)
+            print axis_convert2(axis, position)
         pygame.time.wait(200)
 
 def axis_convert(axis, value):
@@ -29,14 +28,26 @@ def axis_convert(axis, value):
         if value < 0:
             return 2000
         else:
-            return 1000
+            return 0
     offset = [0.4433, 0.4846, -0.6701, 0.9794, 0.8866, 0.4949]
+    #offset = [0.4433, 0.4846, -0.6, 0.9794, 0.8866, 0.4949]
     mults = [1021, 982, -789, 1, 560, 1042]
     value += offset[axis]
     if value < 0 and axis != 2:
         value = 0
     value *= mults[axis]
-    return int(value + 1000)
+    if axis == 2:
+        return int(value * 2 + 65) 
+    value += 1000
+    if axis == 5:
+        print value
+    if value > 2000:
+        return 2000
+    elif value < 1000:
+        return 1000
+    else:
+        return int(value)
+    #return int(value + 1000)
 
 def to16bit(x):
     return (x%(1<<16))
@@ -52,10 +63,10 @@ def sendCommand(serialPort, throttle = 0, roll = 0, pitch = 0, yaw = 0, gear = 0
     msg.append(0)
     msg.append((((throttle >> 8) & 0b00000111)))
     msg.append(to8bit(throttle))
-    msg.append((((pitch >> 8) & 0b00000111 | 0b00001000)))
-    msg.append(to8bit(pitch))
-    msg.append((((roll >> 8) & 0b00000111 | 0b00010000)))
+    msg.append((((roll >> 8) & 0b00000111 | 0b00001000)))
     msg.append(to8bit(roll))
+    msg.append((((pitch >> 8) & 0b00000111 | 0b00010000)))
+    msg.append(to8bit(pitch))
     msg.append((((yaw >> 8) & 0b00000111 | 0b00011000)))
     msg.append(to8bit(yaw))
     msg.append((((gear >> 8) & 0b00000111 | 0b00100000)))
@@ -72,16 +83,66 @@ def prog_loop():
         t = []
         pygame.event.get()
         for axis in range(6):
-            t.append(axis_convert(axis, joy.get_axis(axis)))
+            val = axis_convert(axis, joy.get_axis(axis))
+            t.append(val)
+        print t[3]
         sendCommand(serialPort = ser, throttle = t[2], roll = t[0], pitch =
-                t[1], yaw = t[5], gear = t[4], aux1 = t[3], aux2 = 0)
+                t[1], yaw = t[5], gear = t[3], aux1 = 0, aux2 = 0)
+        #pygame.time.wait(50)
 
 def step():
     fTime = 0
     while True:
-        print "Throttle value: ", fTime * 40
-        sendCommand(throttle = fTime * 40)
+        tVal = fTime * 40
+        print "Throttle value: ", tVal
+        sendCommand(serialPort = ser, throttle = tVal, gear = 2000)
         if fTime > 40:
             fTime = 0
         else:
             fTime += 1
+        pygame.time.wait(50)
+
+def axis_convert2(axis, value):
+    if axis == 3:
+        if value < 0:
+            return 2000
+        else:
+            return 0
+    #offset = [0.4433, 0.4846, -0.6701, 0.9794, 0.8866, 0.4949]
+    #offset = [0.4433, 0.4846, -0.6, 0.9794, 0.8866, 0.4949]
+    mults = [-1021 / 2, 982 / 2, -789, 1, 560, -1042]
+    #value += offset[axis]
+    value *= mults[axis]
+#    if axis == 2 or 1:
+    value = int(value * 2) 
+    if value > 1000:
+        value = 1000
+    elif value < -1000:
+        value = -1000
+    if axis == 5:
+        value = value / 2
+        print value + 900
+        return int(value + 900)
+    else:
+        return int(value + 1024)
+
+def prog_loop2():
+    while True:
+        t = []
+        pygame.event.get()
+        for axis in range(6):
+            val = axis_convert2(axis, joy.get_axis(axis))
+            t.append(val)
+        print t[3]
+        sendCommand(serialPort = ser, throttle = t[2], roll = t[0], pitch =
+                t[1], yaw = t[5], gear = t[3], aux1 = 0, aux2 = 0)
+        pygame.time.wait(20)
+
+def quadprint2():
+    while True:
+        axislist = []
+        pygame.event.get()
+        for axis in range(joy.get_numaxes()):
+            position = joy.get_axis(axis)
+            print axis_convert2(axis, position)
+        pygame.time.wait(200)
